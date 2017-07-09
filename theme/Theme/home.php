879db0e4aa36fca964669e6a333357bd21258504
KOHEGYCb <dimaushenko@mail.ru>
2017-07-09 22:38:07 +0300

    change menu, logo, add form on home page

--- theme/Theme/home.php
+++ theme/Theme/home.php
@@ -13,16 +13,17 @@
     <link href="<?php get_theme_url(); ?>/css/style.css" rel="stylesheet">
     <link href="<?php get_theme_url(); ?>/css/menu.css" rel="stylesheet">
     <link href="<?php get_theme_url(); ?>/css/logo.css" rel="stylesheet">
-    <link href="<?php get_theme_url(); ?>/css/slider.css" rel="stylesheet">
+    <link href="<?php get_theme_url(); ?>/css/slider.css" rel="stylesheet" media="screen">
+    <link href="<?php get_theme_url(); ?>/css/form.css" rel="stylesheet">
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
     <script type="text/javascript" src="<?php get_theme_url(); ?>/js/slider.js"></script>
 </head>
 
 <body>
-    <?php include('skeleton/menu.php'); ?>
 
     <!-- page content-->
     <div class="content">
+       <?php include('skeleton/menu.php'); ?>
         <?php include('skeleton/logo.php'); ?>
 
         <div class="slider-box">
@@ -30,8 +31,6 @@
                 <img src="http://localhost/CapitalGroup/data/uploads/slider/slide_01.jpg" width="900" alt="">
                 <img src="http://localhost/CapitalGroup/data/uploads/slider/slide_02.jpg" width="900" alt="">
                 <img src="http://localhost/CapitalGroup/data/uploads/slider/slide_03.jpg" width="900" alt="">
-                <img src="http://localhost/CapitalGroup/data/uploads/slider/slide_04.jpg" width="900" alt="">
-                <img src="http://localhost/CapitalGroup/data/uploads/slider/slide_05.jpg" width="900" alt="">
             </div>
             <div class="prev"></div>
             <div class="next"></div>
@@ -41,6 +40,29 @@
     get_page_content();
 
     ?>
+    <hr>
+    <form class="contact_form" action="#" method="post" name="contact_form">
+    <ul>
+        <li>
+             <h2>Напишите нам</h2>
+        </li>
+        <li>
+            <label for="name">Имя:</label>
+            <input type="text"  placeholder="Ваше имя" required />
+        </li>
+        <li>
+            <label for="email">Email:</label>
+            <input type="email" name="email" placeholder="Email" required />
+        </li>
+        <li>
+            <label for="message">Сообщение:</label>
+            <textarea name="message" cols="40" rows="6" required ></textarea>
+        </li>
+        <li>
+        	<button class="submit" type="submit">Отправить</button>
+        </li>
+    </ul>
+</form>
     </div>
     <!-- page content end-->
 
